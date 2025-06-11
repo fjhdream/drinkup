7c06a4b962c445831fdc136d67cf0d7fd4d77969
lollapalooza <fjh_dream@163.com>
2025-06-11 22:08:27 +0800

    ✨ feat(bartender): 增加随机主题选择功能
    - 添加SecureRandom实例用于随机选择主题
    - 当输入为空或无效时，随机返回一个已有的主题

--- src/main/java/cool/drinkup/drinkup/workflow/internal/service/bartender/theme/ThemeEnum.java
+++ src/main/java/cool/drinkup/drinkup/workflow/internal/service/bartender/theme/ThemeEnum.java
@@ -1,5 +1,7 @@
 package cool.drinkup.drinkup.workflow.internal.service.bartender.theme;
 
+import java.security.SecureRandom;
+
 import jakarta.annotation.Nullable;
 
 public enum ThemeEnum {
@@ -9,9 +11,11 @@ public enum ThemeEnum {
     RANDOM,
     ;
 
+    private static final SecureRandom SECURE_RANDOM = new SecureRandom();
     public static @Nullable ThemeEnum fromValue(String value) {
         if (value == null || value.isEmpty()) {
-            return null; // 返回null表示没有主题
+            // 随机一个已有的theme
+            return ThemeEnum.values()[SECURE_RANDOM.nextInt(ThemeEnum.values().length)];
         }
         return ThemeEnum.valueOf(value.toUpperCase());
     }
